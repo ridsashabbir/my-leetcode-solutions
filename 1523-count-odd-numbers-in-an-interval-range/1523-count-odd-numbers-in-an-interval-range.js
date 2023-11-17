a7dd1b7df4b6044cec45b1813920e5f8ad19c31b
Ridsa Shabbir <81819648+ridsashabbir@users.noreply.github.com>
2023-11-17 22:57:40 +0500

    Time: 59 ms (70.69%), Space: 41.8 MB (53.54%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    if(low % 2 === 0 && high % 2 === 0){
+        return (high-low)/2
+    }
+    else{
+        return (high-low+1)/2
+    }
+};
