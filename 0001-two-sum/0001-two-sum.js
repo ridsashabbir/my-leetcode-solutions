18c4989b5d2d597f99a8a8103bb98152df767873
Ridsa Shabbir <81819648+ridsashabbir@users.noreply.github.com>
2023-11-27 22:57:12 +0500

    Time: 56 ms (77.04%), Space: 42.9 MB (36.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,28 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let sum = 0 ;
-    for(let i=0; i<nums.length - 1 ; i++){
-        for(let j = i+1; j< nums.length ; j++){
-            sum = nums[i] + nums[j];
-            if(sum === target){
-                return [i, j];
-            }
+    // solution 1: O(n^2)
+    // let sum = 0 ;
+    // for(let i=0; i<nums.length - 1 ; i++){
+    //     for(let j = i+1; j< nums.length ; j++){
+    //         sum = nums[i] + nums[j];
+    //         if(sum === target){
+    //             return [i, j];
+    //         }
+    //     }
+    // }
+    // return [];
+    
+    
+    // solution 2: O(n)
+    const map = {};
+    for(let i=0; i< nums.length; i++){
+        const complement = target - nums[i];
+        if(complement in map){
+            return [map[complement], i];
         }
+        // store the current number and its index in hash map
+        map[nums[i]] = i;
     }
     return [];
 };
