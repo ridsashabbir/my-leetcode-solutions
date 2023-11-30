7f0cebcd6aa3c6eb37b43fc6dd7e3af1442ac45e
Ridsa Shabbir <81819648+ridsashabbir@users.noreply.github.com>
2023-11-30 22:16:39 +0500

    Time: 61 ms (8.78%), Space: 41.6 MB (79.67%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,15 +3,14 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-      let carry = 1; // this is the digit which is to be added
-    
-    for(let i=digits.length-1 ; i>=0 && carry>0; i--){
-        const sum = digits[i]+ carry;  // add one 
-        digits[i] = sum%10;// update the current digit
-        carry = Math.floor(sum/10); // update carry for the next iteration
+    let carry = 1;
+    for(let i=digits.length-1; i>=0 && carry>0 ; i--){
+        let sum = digits[i] + carry;
+        digits[i] = sum % 10; // update the current digit after adding 1 carry
+        carry = Math.floor(sum / 10); //update carry for next iteration
     }
-    if (carry > 0) {
-        digits.unshift(carry);
+    if(carry > 0){
+        digits.unshift(carry); // if carry remains then add it to the start of the array
     }
     return digits;
 };
