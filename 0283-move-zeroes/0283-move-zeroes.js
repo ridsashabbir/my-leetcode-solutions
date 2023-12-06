f9cb5d457e93ef6dac75073668d9dae3e628bfc5
Ridsa Shabbir <81819648+ridsashabbir@users.noreply.github.com>
2023-12-06 18:55:56 +0500

    Time: 81 ms (38.22%), Space: 47 MB (12.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,17 +3,25 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let nonZero = [];
+    // let nonZero = [];
+    // for(let i=0; i<nums.length; i++){
+    //     if(nums[i] !== 0){
+    //         nonZero.push(nums[i]);
+    //     }
+    // }
+    // while(nonZero.length < nums.length){
+    //     nonZero.push(0);
+    // }
+    // //as we cannot make copy of the array so let's shift elements in the main array
+    // for(let i=0; i<nums.length; i++){
+    //     nums[i] = nonZero[i];
+    // }
+    
+    let nonZero = 0;
     for(let i=0; i<nums.length; i++){
         if(nums[i] !== 0){
-            nonZero.push(nums[i]);
+            [nums[i], nums[nonZero]] = [nums[nonZero], nums[i]];
+            nonZero++;
         }
-    }
-    while(nonZero.length < nums.length){
-        nonZero.push(0);
-    }
-    //as we cannot make copy of the array so let's shift elements in the main array
-    for(let i=0; i<nums.length; i++){
-        nums[i] = nonZero[i];
     }
 };
