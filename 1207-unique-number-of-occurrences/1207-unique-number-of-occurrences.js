01188e4b7d97d737e87374407b6867f6ec9e894b
Ridsa Shabbir <81819648+ridsashabbir@users.noreply.github.com>
2023-12-16 16:17:11 +0500

    Time: 64 ms (9.43%), Space: 43.6 MB (24.77%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -3,27 +3,34 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
-    const occurences = {};
-    const frequencies = []
-    for(let i=0; i<arr.length; i++){
-        const num = arr[i];
-        if(occurences[num]){
-            occurences[num] += 1
-        } else {
-            occurences[num] = 1;
-        }
+    let occurences = {};
+    for(let el of arr){
+        occurences[el] = occurences[el] ? occurences[el]+1 : 1
     }
-    for(const key in occurences){
-        if(occurences.hasOwnProperty(key)){
-            frequencies.push(occurences[key])
-        }
-    }
-    for(let i=0;i<frequencies.length; i++){
-        for(let j=i+1; j<frequencies.length; j++){
-            if(frequencies[i] === frequencies[j]){
-                return false;
-            }
-        }
-    }
-    return true;
+    return Object.values(occurences).length === new Set(Object.values(occurences)).size
+    
+    
+    // const occurences = {};
+    // const frequencies = []
+    // for(let i=0; i<arr.length; i++){
+    //     const num = arr[i];
+    //     if(occurences[num]){
+    //         occurences[num] += 1
+    //     } else {
+    //         occurences[num] = 1;
+    //     }
+    // }
+    // for(const key in occurences){
+    //     if(occurences.hasOwnProperty(key)){
+    //         frequencies.push(occurences[key])
+    //     }
+    // }
+    // for(let i=0;i<frequencies.length; i++){
+    //     for(let j=i+1; j<frequencies.length; j++){
+    //         if(frequencies[i] === frequencies[j]){
+    //             return false;
+    //         }
+    //     }
+    // }
+    // return true;
 };
